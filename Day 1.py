c3a8bc7c455efbd8d8fde4cf12cf9d103d83a3fb
Sumit <70812063+Sumit28121996@users.noreply.github.com>
2024-05-13 01:54:44 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,153 @@
+#  Sorting of Number
+'''
+lst = [2,3,6,5]
+for i in range(len(lst)):
+    for j in range(i+1,len(lst)):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
+
+print(lst)
+
+'''
+
+
+#
+# #reverse of numbers/txt
+'''
+name = 'raj'
+name_rev = name[::-1]
+print(name_rev)
+
+age = 82
+divisor = age//10
+reminder = age % 10'''
+
+
+#Palindrome
+'''number = input("Enter any value:")
+num_reverse = number[::-1]
+if number == num_reverse:
+    print("PALINDROME")
+
+else:
+    print("Not Palindrome")'''
+
+
+# Python program to interchange first and last elements in a list
+
+list = [1, 2, 33, 4]
+# swaping
+list[0], list[-1] = list[-1], list[0]
+
+print(list)
+
+# #Python program to swap two elements in a list
+# #Python | Ways to find length of list
+# #Maximum of two numbers in Python
+# #Minimum of two numbers in Python
+#
+# Python List Exercises
+# Python program to interchange first and last elements in a list
+# Python program to swap two elements in a list
+# Python | Ways to find length of list
+# Maximum of two numbers in Python
+# Minimum of two numbers in Python
+# >> More Programs on List
+#
+# Python String Exercises
+# Python program to check whether the string is Symmetrical or Palindrome
+# Reverse words in a given String in Python
+# Ways to remove i’th character from string in Python
+# Find length of a string in python (4 ways)
+# Python program to print even length words in a string
+# >> More Programs on String
+#
+# Python Tuple Exercises
+# Python program to Find the size of a Tuple
+# Python – Maximum and Minimum K elements in Tuple
+# Python – Sum of tuple elements
+# Python – Row-wise element Addition in Tuple Matrix
+# Create a list of tuples from given list having number and its cube in each tuple
+# >> More Programs on Tuple
+#
+# Python Dictionary Exercises
+# Python | Sort Python Dictionaries by Key or Value
+# Handling missing keys in Python dictionaries
+# Python dictionary with keys having multiple inputs
+# Python program to find the sum of all items in a dictionary
+# Python program to find the size of a Dictionary
+# >> More Programs on Dictionary
+#
+# Python Set Exercises
+# Find the size of a Set in Python
+# Iterate over a set in Python
+# Python – Maximum and Minimum in a Set
+# Python – Remove items from Set
+# Python – Check if two lists have atleast one element common
+# >> More Programs on Sets
+#
+# Python Matrix Exercises
+# Python – Assigning Subsequent Rows to Matrix first row elements
+# Adding and Subtracting Matrices in Python
+# Python – Group similar elements into Matrix
+# Python – Row-wise element Addition in Tuple Matrix
+# Create an n x n square matrix, where all the sub-matrix have the sum of opposite corner elements as even
+# >> More Programs on Matrices
+#
+# Python Functions Exercises
+# How to get list of parameters name from a function in Python?
+# How to Print Multiple Arguments in Python?
+# Python program to find the power of a number using recursion
+# Sorting objects of user defined class in Python
+# Functions that accept variable length key value pair as arguments
+# >> More Programs on Functions
+#
+# Python Lambda Exercises
+# Lambda with if but without else in Python
+# Python | Sorting string using order defined by another string
+# Python | Find fibonacci series upto n using lambda
+# Python program to count Even and Odd numbers in a List
+# Python | Find the Number Occurring Odd Number of Times using Lambda expression and reduce function
+# >> More Programs on Lambda
+#
+#
+# Python Pattern printing Exercises
+# Program to print half Diamond star pattern
+# Programs for printing pyramid patterns in Python
+# Program to print the diamond shape
+# Python | Print an Inverted Star Pattern
+# Python Program to print digit pattern
+# >> More Programs on Python Pattern Printing
+#
+# Python DateTime Exercises
+# Python program to get Current Time
+# Get Yesterday’s date using Python
+# Python program to print current year, month and day
+# Python – Convert day number to date in particular year
+# Get Current Time in different Timezone using Python
+# >> More Programs on DateTime
+#
+# NumPy Exercises
+# How to create an empty and a full NumPy array?
+# Create a Numpy array filled with all zeros
+# Create a Numpy array filled with all ones
+# Replace NumPy array elements that doesn’t satisfy the given condition
+# Get the maximum value from given matrix
+# >> More Programs on NumPy
+#
+# Pandas Exercises
+# Make a Pandas DataFrame with two-dimensional list | Python
+# How to iterate over rows in Pandas Dataframe
+# Create a pandas column using for loop
+# Create a Pandas Series from array
+# Pandas | Basic of Time Series Manipulation
+# >> More Programs on Python Pandas
+#
+# Python Projects
+# Number guessing game in Python
+# 2048 Game in Python
+# Get Live Weather Desktop Notifications Using Python
+# 8-bit game using pygame
+# Tic Tac Toe GUI In Python using PyGame
+# >> More Projects in Python
+#
